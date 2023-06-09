5ec922f5a6b9c52b5635dc36ab7bbcea634b89dc
Ty-Watson <Ty.Watson77@outlook.com>
2023-06-08 23:59:07 -0500

    update accept api route friend to friends

--- app/api/friends/accept/route.js
+++ app/api/friends/accept/route.js
@@ -36,7 +36,7 @@ export async function POST(req){
 
         await db.sadd(`user:${session.user.id}:friends`, idToAdd)
 
-        await db.sadd(`user:${idToAdd}:friend`, session.user.id)
+        await db.sadd(`user:${idToAdd}:friends`, session.user.id)
 
         //await db.srem(`user:${idToAdd}:outbound_friend_requests`, session.user.id)
 
